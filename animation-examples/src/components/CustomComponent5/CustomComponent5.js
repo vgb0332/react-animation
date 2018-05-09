985fc5061c5eb593ce0b084ca284adfc4e67f7b8
mjung <vgb000123@gmail.com>
2018-05-09 21:31:12 +0900

    example5 modified

--- animation-examples/src/components/CustomComponent5/CustomComponent5.js
+++ animation-examples/src/components/CustomComponent5/CustomComponent5.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { VelocityComponent } from 'velocity-react';
 
-const VelocityLetter = ({ letter }) => (
+const VelocityLetter = ({ duration, letter }) => (
   <VelocityComponent
     runOnMount
     animation={{ opacity: 1, marginTop: 0 }}
-    duration={500}
+    duration={duration ? duration: 1000}
   >
     <p style={styles.letter}>{letter}</p>
   </VelocityComponent>
@@ -14,14 +14,26 @@ const VelocityLetter = ({ letter }) => (
 class CustomComponent5 extends Component {
   state = {
     letters: [],
+    anotherLetters: []
   }
   onChange = (e) => {
     const letters = e.target.value.split('');
     const arr = []
     letters.forEach((l, i) => {
-      arr.push(<VelocityLetter key={i} letter={l} />)
+      const r = Math.floor((Math.random() * 1000) + 1);
+      arr.push(<VelocityLetter key={i} duration={r} letter={l} />)
     })
     this.setState(() => ({ letters: arr }))
+  }
+
+  onAnotherChange = (e) => {
+    const letters = e.target.value.split('');
+    const arr = []
+    letters.forEach((l, i) => {
+      const r = Math.floor((Math.random() * 1000) + 1);
+      arr.push(<VelocityLetter key={i} letter={l} />)
+    })
+    this.setState(() => ({ anotherLetters: arr }))
   }
 
   render() {
@@ -32,6 +44,12 @@ class CustomComponent5 extends Component {
           <div style={styles.letters}>
             {
               this.state.letters
+            }
+          </div>
+          <input onChange={this.onAnotherChange} style={styles.input} />
+          <div style={styles.letters}>
+            {
+              this.state.anotherLetters
             }
           </div>
         </div>
